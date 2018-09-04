5d0bc333a6a8adb7cadb6bb78a06c9cbbe8261dd
anamritraj <anandamritraj904@gmail.com>
2018-09-04 20:06:13 +0530

    Fix file-name typo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import CalendarHeatMap from './CalendarHeatMap/CalendarHeatMap';
+import CalendarHeatMap from './CalenderHeatMap/CalendarHeatMap';
 
 class App extends Component {
   render() {
